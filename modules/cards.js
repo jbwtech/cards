e985de64b4e7ebf4a1b77f505d82fe0e70abc318
Brent Williams <jbwtech@gmail.com>
2023-01-27 16:38:33 -0500

    Minor changes to Card class

--- modules/cards.js
+++ modules/cards.js
@@ -5,23 +5,6 @@ const suit = ["s", "h", "d", "c" ];
 
 
 
-function InitialDeal( numberOfCards, hands, dealer ) {
-
-    for( i=0; i < numberOfCards; i++ ) {
-        // Deal Crads to players
-        hands.forEach((hand) => { 
-            hand.cards.push(shoe.pop());
-        })
-
-        if(dealer !== null) {
-            // Deal Cards to delaer
-            dealer.cards.push(shoe.pop());
-        }        
-    }
-
-    return;
-}
-
 module.exports = {
     InitialDeal
 }
