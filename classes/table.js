c3f548a85d1f049aa5fc1c31c450e91ec0d729d3
Brent Williams <jbwtech@gmail.com>
2023-01-28 15:04:22 -0500

    More modsClass for Table

---
+++ classes/table.js
@@ -0,0 +1,30 @@
+
+const { Shoe } = require('./shoe.js');
+const { Dealer, Player } = require('./player.js');
+
+const minimumBet = 5;
+
+class Table {
+
+    #shoe;
+    #delaer;
+    #seats;
+
+    constructor() {
+        this.shoe = new Shoe(8);
+        this.dealer = new Dealer();
+        this.seats = new Array();
+    }
+
+    AddPlayer(player,chips) {
+        this.seats.push(new Player(player, chips));
+    }
+
+    ShowPlayers() {
+        this.seats.forEach((player) => {
+            console.log(player);
+        });
+    }
+}
+
+module.exports = { Table };
