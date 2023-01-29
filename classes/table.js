618ad5f3e08005d31a393e53c90e7332256872a6
Brent Williams <jbwtech@gmail.com>
2023-01-28 22:33:03 -0500

    Added id, made all properties private

--- classes/table.js
+++ classes/table.js
@@ -5,23 +5,23 @@ const { Dealer, Player } = require('./player.js');
 const minimumBet = 5;
 
 class Table {
-
+    #id;
     #shoe;
-    #delaer;
+    #dealer;
     #seats;
 
     constructor() {
-        this.shoe = new Shoe(8);
-        this.dealer = new Dealer();
-        this.seats = new Array();
+        this.#shoe = new Shoe(8);
+        this.#dealer = new Dealer();
+        this.#seats = new Array();
     }
 
     AddPlayer(player,chips) {
-        this.seats.push(new Player(player, chips));
+        this.#seats.push(new Player(player, chips));
     }
 
     ShowPlayers() {
-        this.seats.forEach((player) => {
+        this.#seats.forEach((player) => {
             console.log(player);
         });
     }
